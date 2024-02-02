d713f06425d690253f223adb78d7fcfea0266016
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2024-02-02 17:05:37 -0300

    Organizando os arquivos

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ model=YOLO('best.pt')
 video = cv.VideoCapture('cr.mp4')
 
 def generate_frames():
-    
-
     box_entry_time = {}
+
+    time_account_by_id = {}
     while True:
         success, frame = video.read()
         
@@ -32,6 +32,53 @@ def generate_frames():
                     qtd_clientes += 1
                     # print(class_name)
                     box_id = int(result.boxes[i].id)
+                    list_de_box_ids = [4, 5]
+                    dicionario = {1, 4, 5}
+
+                    # Rotina para gerar informações sobre o tempo de atendimento do cliente.
+                    current_time = datetime.now()
+                    if box_id not in time_account_by_id:
+                        time_account_by_id[box_id] = [
+                            [current_time.hour, current_time.minute, current_time.second, current_time.microsecond // 1000,current_time.day, current_time.month, current_time.year], 
+                            [current_time.hour, current_time.minute, current_time.second, current_time.microsecond // 1000, current_time.day, current_time.month, current_time.year],
+                            [current_time.hour, current_time.minute, current_time.second, current_time.microsecond // 1000, current_time.day, current_time.month, current_time.year],  
+                            0
+                            ]
+                    else:
+                        time_account_by_id[box_id][2] = [current_time.hour, current_time.minute, current_time.second, current_time.microsecond // 1000, current_time.day, current_time.month, current_time.year]
+
+                        diff_seconds = (
+                            datetime(current_time.year, current_time.month, current_time.day,
+                                    time_account_by_id[box_id][2][0], time_account_by_id[box_id][2][1], time_account_by_id[box_id][2][2],
+                                    time_account_by_id[box_id][2][3])
+                            - datetime(current_time.year, current_time.month, current_time.day,
+                                        time_account_by_id[box_id][0][0], time_account_by_id[box_id][0][1], time_account_by_id[box_id][0][2],
+                                        time_account_by_id[box_id][0][3])
+                        ).total_seconds()
+
+                        hours, remainder = divmod(diff_seconds, 3600)
+                        minutes, seconds = divmod(remainder, 60)
+                        seconds, milliseconds = divmod(int(seconds * 1000), 1000)
+
+                        # time_account_by_id[box_id][3] = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+                        time_account_by_id[box_id][3] = f"{int(hours)}h {int(minutes)}m {int(seconds)}s {milliseconds}ms"
+                    
+                        if time_account_by_id[box_id][2] != time_account_by_id[box_id][1]:
+                            print('hahahaha')
+                            print(box_id)
+                            print(time_account_by_id[box_id][0])
+                            print(time_account_by_id[box_id][1])
+                            print(time_account_by_id[box_id][2])
+                            print(time_account_by_id[box_id][3])
+                            print('hahahaha')
+                            time_account_by_id[box_id][1] = time_account_by_id[box_id][2]
+                        elif time_account_by_id[box_id][2] == time_account_by_id[box_id][1]:
+                            # escrever aqui a rotina de CSV
+                            # time_account_by_id.pop([box_id])
+                            print('hihihihi')
+
+
+                    # Rotina para plotar as caixas e o tempo decorrido
                     box_coordinates = result.boxes[i].xyxy[0].cpu().numpy()
                     # Verifique se o ID já está no dicionário
                     if box_id not in box_entry_time:
@@ -56,6 +103,7 @@ def generate_frames():
                 else:
                     qtd_atendentes += 1
 
+            print(time_account_by_id)
             frame_predicted = result.plot(boxes=True, conf=False) # Track
 
             _, buffer = cv.imencode('.jpg', frame_predicted)
